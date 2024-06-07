d5589a7b02ffc1926db382fed952171011f14a9c
DasunWiduranga <dasun.20200233@iit.ac.lk>
2024-06-07 22:29:42 +0530

    Adding mui styles

--- task_frontend/src/components/TaskDashboard.js
+++ task_frontend/src/components/TaskDashboard.js
@@ -1,29 +1,73 @@
-import React, { useState } from 'react';
-import TaskList from './TaskList';
-import TaskForm from './TaskForm';
+import React, { useState } from "react";
+import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+import {
+  Grid,
+  Typography,
+  InputLabel,
+  Select,
+  FormControl,
+  MenuItem,
+  Divider,
+} from "@mui/material";
 
 function TaskDashboard({ tasks, addTask, updateTask, deleteTask }) {
-  const [filter, setFilter] = useState('all');
+  const [filter, setFilter] = useState("all");
 
-  const filteredTasks = tasks.filter(task => 
-    filter === 'all' || task.status === filter
+  const filteredTasks = tasks.filter(
+    (task) => filter === "all" || task.status === filter
   );
 
   return (
-    <div>
-      <h1>Task Management Dashboard</h1>
-      <TaskForm addTask={addTask} />
-      <div>
-        <label>Filter: </label>
-        <select onChange={e => setFilter(e.target.value)}>
-          <option value="all">All</option>
-          <option value="pending">Pending</option>
-          <option value="in progress">In Progress</option>
-          <option value="completed">Completed</option>
-        </select>
-      </div>
-      <TaskList tasks={filteredTasks} updateTask={updateTask} deleteTask={deleteTask} />
-    </div>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="stretch"
+      px={3}
+    >
+      <Grid item xs={12} my={4}>
+        <Typography variant="h4" style={{ fontWeight: "bold" }}>
+          Task Management Dashboard
+        </Typography>
+      </Grid>
+      <Grid item xs={12} my={4}>
+        <TaskForm addTask={addTask} />
+      </Grid>
+      <Divider></Divider>
+      <Grid
+        item
+        container
+        direction="row"
+        justifyContent="center"
+        alignContent="center"
+        alignItems="center"
+        py={2}
+      >
+        <Grid item container alignItems="flex-start" xs={1.2}>
+          <InputLabel>Filter:</InputLabel>
+        </Grid>
+        <Grid item xs={3}>
+          <FormControl fullWidth variant="outlined">
+            <Select
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+              style={{ height: 30 }}
+            >
+              <MenuItem value="all">All</MenuItem>
+              <MenuItem value="pending">Pending</MenuItem>
+              <MenuItem value="in progress">In Progress</MenuItem>
+              <MenuItem value="completed">Completed</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <TaskList
+          tasks={filteredTasks}
+          updateTask={updateTask}
+          deleteTask={deleteTask}
+        />
+      </Grid>
+    </Grid>
   );
 }
 
--- task_frontend/src/components/TaskForm.js
+++ task_frontend/src/components/TaskForm.js
@@ -1,46 +1,122 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import {
+  Grid,
+  TextField,
+  Select,
+  MenuItem,
+  Button,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
 
 function TaskForm({ addTask }) {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [assignedTo, setAssignedTo] = useState('');
-  const [status, setStatus] = useState('pending');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [assignedTo, setAssignedTo] = useState("");
+  const [status, setStatus] = useState("pending");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newTask = { title, description, assigned_to: assignedTo, status };
     addTask(newTask);
-    setTitle('');
-    setDescription('');
-    setAssignedTo('');
-    setStatus('pending');
+    setTitle("");
+    setDescription("");
+    setAssignedTo("");
+    setStatus("pending");
   };
+  const labelColumnRatio = 1.2;
+  const inputColumnRatio = 4;
 
   return (
     <form onSubmit={handleSubmit}>
-      <input 
-        type="text" 
-        placeholder="Title" 
-        value={title} 
-        onChange={(e) => setTitle(e.target.value)} 
-      />
-      <textarea 
-        placeholder="Description" 
-        value={description} 
-        onChange={(e) => setDescription(e.target.value)} 
-      />
-      <input 
-        type="text" 
-        placeholder="Assigned To" 
-        value={assignedTo} 
-        onChange={(e) => setAssignedTo(e.target.value)} 
-      />
-      <select value={status} onChange={(e) => setStatus(e.target.value)}>
-        <option value="pending">Pending</option>
-        <option value="in progress">In Progress</option>
-        <option value="completed">Completed</option>
-      </select>
-      <button type="submit">Add Task</button>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignContent="center"
+        alignItems="center"
+      >
+        <Grid item container justifyContent="center" alignItems="center" my={1}>
+          <Grid item xs={labelColumnRatio} container alignItems="flex-start">
+            <InputLabel>Title</InputLabel>
+          </Grid>
+          <Grid item xs={inputColumnRatio}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item container justifyContent="center" alignItems="center" my={1}>
+          <Grid item xs={labelColumnRatio} container alignItems="flex-start">
+            <InputLabel>Description</InputLabel>
+          </Grid>
+          <Grid item xs={inputColumnRatio}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              multiline
+              rows={4}
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item container justifyContent="center" alignItems="center" my={1}>
+          <Grid item xs={labelColumnRatio} container alignItems="flex-start">
+            <InputLabel>Assigned To</InputLabel>
+          </Grid>
+          <Grid item xs={inputColumnRatio}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              placeholder="Assigned To"
+              value={assignedTo}
+              onChange={(e) => setAssignedTo(e.target.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item container justifyContent="center" alignItems="center" my={1}>
+          <Grid item xs={labelColumnRatio} container alignItems="flex-start">
+            <InputLabel>Status</InputLabel>
+          </Grid>
+          <Grid item xs={inputColumnRatio}>
+            <FormControl fullWidth variant="outlined">
+              <Select
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                style={{ height: 30 }}
+              >
+                <MenuItem value="pending">Pending</MenuItem>
+                <MenuItem value="in progress">In Progress</MenuItem>
+                <MenuItem value="completed">Completed</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <Grid item container justifyContent="center" my={1}>
+          <Grid
+            item
+            xs={labelColumnRatio + inputColumnRatio}
+            container
+            justifyContent="center"
+          >
+            <Grid item container justifyContent="flex-end">
+              <Button type="submit" variant="contained" color="primary">
+                Add Task
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </form>
   );
 }
